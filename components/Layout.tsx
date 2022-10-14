a65b66484b0a6a0e37d225884fde78cb31802450
guiguididi <guiguididi@imac-de-guiguididi-1.home>
2022-10-14 21:04:34 +0200

    add greensock

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react'
-import styled from 'styled-components'
 import { globalRefsContext } from '../context/GlobalRefs'
 import Footer from './Footer'
 import Header from './Header'
